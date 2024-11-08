fe279a15f682b15e3bddf3ed2856308286fbd009
Boamah-Powers <kb4242@nyu.edu>
2024-11-08 00:55:31 -0500

    removed unused data

--- frontend/src/routes/homePage.jsx
+++ frontend/src/routes/homePage.jsx
@@ -5,36 +5,6 @@ import { Suspense } from "react";
 
 function HomePage() {
 	const data = useLoaderData();
-	console.log(data);
-	const musicList = [
-		{
-			name: "olanetsoft",
-			tags: ["jazz", "smooth"],
-			description:
-				"An unthinkable thing to be done to humans To make the description section scrollable while keeping the rest of the layout fixed, you can use overflow-y-auto along with a max-h constraint. This will allow the description to scroll within a set height, while other elements remain unaffected.",
-			title: "Bang Bang",
-			url: "https://res.cloudinary.com/demo/video/upload/dog.mp3",
-			created_at: "2021-10-04T23:30:01.000Z",
-		},
-		{
-			name: "olanetsoft",
-			tags: ["jazz", "smooth"],
-			description:
-				"An unthinkable thing to be done to humans To make the description section scrollable while keeping the rest of the layout fixed, you can use overflow-y-auto along with a max-h constraint. This will allow the description to scroll within a set height, while other elements remain unaffected.",
-			title: "Bang Bang",
-			url: "https://res.cloudinary.com/demo/video/upload/dog.mp3",
-			created_at: "2021-10-04T23:30:01.000Z",
-		},
-		{
-			name: "olanetsoft",
-			tags: ["jazz", "smooth"],
-			description:
-				"An unthinkable thing to be done to humans To make the description section scrollable while keeping the rest of the layout fixed, you can use overflow-y-auto along with a max-h constraint. This will allow the description to scroll within a set height, while other elements remain unaffected.",
-			title: "Bang Bang",
-			url: "https://res.cloudinary.com/demo/video/upload/dog.mp3",
-			created_at: "2021-10-04T23:30:01.000Z",
-		},
-	];
 
 	return (
 		<div className="p-4 h-full">
