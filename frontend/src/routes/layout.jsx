1f4286a7408401bc1957a712c3b9be85d3b8c031
Boamah-Powers <kb4242@nyu.edu>
2024-11-15 16:44:31 -0500

    fixed import issue

--- frontend/src/routes/layout.jsx
+++ frontend/src/routes/layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import Navbar from "../components/Navbar";
